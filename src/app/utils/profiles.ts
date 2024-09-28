2c4f125e662b69b48be0c8edc3a7de73f4ab411b
Shaamak <shaamak34@gmail.com>
2024-09-28 12:22:07 +0530

    add: shaamak

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "shaamak",
+    name: "shaamak",
+    quote: "I know where you live 🌝",
+  },
 ];
